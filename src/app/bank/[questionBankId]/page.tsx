cb7b6cbbbfbd226b010479fec39a90178d02b082
hlw422 <hlw422@126.com>
2024-12-23 20:52:19 +0800

    增加题库详情页

---
+++ src/app/bank/[questionBankId]/page.tsx
@@ -0,0 +1,52 @@
+import { getQuestionBankVoByIdUsingGet } from "@/api/questionBankController";
+import QuestionBankList from "@/components/QuestionBankList";
+import QuestionList from "@/components/QuestionList";
+import { Avatar, Card, message, Typography } from "antd";
+import Meta from "antd/es/card/Meta";
+import Paragraph from "antd/es/typography/Paragraph";
+import Title from "antd/es/typography/Title";
+import { AxiosResponse } from "axios";
+
+export default async function BankPage({params}) {
+    console.log("params:",params);
+    const {questionBankId} = params;
+    let bank=undefined;
+    try{
+        const res=await getQuestionBankVoByIdUsingGet({
+            id:questionBankId,
+            needQueryQuestionList:true,
+            pageSize:200
+        })
+        bank=res.data;
+        console.log("bank:",bank);
+    }
+    catch(e){
+        message.error("获取题库失败,"+e.message);
+    }
+    if(!bank){
+        return <div>获取题库列表失败，请刷新重试</div>
+    }
+    return(
+        <div id="bankPage" className="max-width-content">
+            <Card>
+                <Meta
+                avatar={<Avatar src={bank.picture} size={72}/>}
+                title={
+                    <Title level={3} style={{marginBottom:0}}>
+                        {bank.title}
+                    </Title>
+                }
+                description={
+                    <Paragraph
+                    type="secondary">
+                        {bank.description}
+                    </Paragraph>
+                }
+                >
+
+                </Meta>
+          <QuestionList questionList={bank.questionPage.records??[]}></QuestionList>
+            </Card>
+        </div>
+    )
+}
